a2391b933e8f971841adde70d80d5751ba2547ba
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-04-21 18:48:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var friends = ['sam', 'john', 'ramy'];
+
+function setup() {
+  
+  console.log("Hi ",friends[1],"!Please grace us with your presence at my birthday celebration");
+}
+
+function draw() {
+  background(220);
+}
