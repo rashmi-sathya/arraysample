ca9cfb713364bdeb4cb69a1fd91c6580c115e2c8
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-03 20:58:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var friends = ['sam', 'john', 'ramy'];
 
 function setup() {
-  
+  coonsole.log(friends);
   console.log("Hi ",friends[1],"!Please grace us with your presence at my birthday celebration");
 }
 
